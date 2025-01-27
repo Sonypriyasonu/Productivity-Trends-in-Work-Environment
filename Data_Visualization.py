5cd699dce7a834af3615c60e84c88de0c646585a
Pothumudi Sony Priya <61133478+Sonypriyasonu@users.noreply.github.com>
2025-01-27 09:42:59 +0530

    Update Data_Visualization.py

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 # Function to visualize "Performance Score vs Department"
 def performance_score_vs_department(df):
@@ -67,3 +69,87 @@ def number_of_employees_by_education_level(df):
     plt.xlabel("Education Level")
     plt.ylabel("Number of Employees")
     return plt
+
+def data_visualization():
+    st.title("Data Visualization")
+    st.markdown("""
+    **Note:** 
+     Please ensure that the dataset contains the following columns: 
+    
+    Employee_ID, Department, Gender, Age, Job_Title, Years_At_Company, Education_Level, Performance_Score, Monthly_Salary, Work_Hours_Per_Week, Projects_Handled, Overtime_Hours, Sick_Days, Remote_Work_Frequency, Team_Size, Training_Hours, Promotions, Employee_Satisfaction_Score.") 
+    
+    Missing any of these columns may result in incomplete or inaccurate analysis.
+    """)   
+    uploaded_file = st.file_uploader("Upload Inference Data (CSV)", type="csv")
+       
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        chart_type = st.selectbox('Choose a Chart:', 
+                                ['Performance Score vs Department', 
+                                 'Performance Score vs Gender', 
+                                 'Performance Score vs Education Level', 
+                                 'Number of Employees by Education Level', 
+                                 'Employee Satisfaction vs Performance Score',
+                                 'Years at Company vs Monthly Salary', 
+                                 'Correlation Matrix', 'All Visualizations'])
+        
+        if chart_type == 'Performance Score vs Department':
+            st.subheader("Performance Score vs Department")
+            fig = performance_score_vs_department(df)
+            st.pyplot(fig)
+
+        elif chart_type == 'Performance Score vs Gender':
+            st.subheader("Performance Score vs Gender")
+            fig = performance_score_vs_gender(df)
+            st.pyplot(fig)
+        
+        elif chart_type == 'Performance Score vs Education Level':
+            st.subheader("Performance Score vs Education Level")
+            fig = performance_score_vs_education_level(df)
+            st.pyplot(fig)
+        
+        elif chart_type == 'Number of Employees by Education Level':
+            st.subheader("Number of Employees by Education Level")
+            fig = number_of_employees_by_education_level(df)
+            st.pyplot(fig)
+        
+        elif chart_type == 'Employee Satisfaction vs Performance Score':
+            st.subheader("Employee Satisfaction vs Performance Score")
+            fig = employee_satisfaction_vs_performance(df)
+            st.pyplot(fig)
+        
+        elif chart_type == 'Years at Company vs Monthly Salary':
+            st.subheader("Years at Company vs Monthly Salary")
+            fig = years_at_company_vs_salary(df)
+            st.pyplot(fig)
+        
+        elif chart_type == 'Correlation Matrix':
+            st.subheader("Correlation Matrix of Numeric Features")
+            fig = correlation_matrix(df)
+            st.pyplot(fig)
+        
+        elif chart_type == "All Visualizations":
+            st.subheader("All Visualizations")
+            fig = performance_score_vs_department(df)
+            st.pyplot(fig)
+            
+            fig = performance_score_vs_gender(df)
+            st.pyplot(fig)
+            
+            fig = performance_score_vs_education_level(df)
+            st.pyplot(fig)
+            
+            fig = number_of_employees_by_education_level(df)
+            st.pyplot(fig)
+            
+            fig = employee_satisfaction_vs_performance(df)
+            st.pyplot(fig)
+            
+            fig = years_at_company_vs_salary(df)
+            st.pyplot(fig)
+            
+            fig = correlation_matrix(df)
+            st.pyplot(fig)
+    else:
+        st.warning("Please upload a CSV file to proceed.")
+            
